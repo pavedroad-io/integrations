d5f019c67b691b94ee6f4ddfdf3b1687ce193bbe
John Scharber <john@pavedroad.io>
2020-08-19 13:10:43 -0700

    Badges (#7)
    Add FOSSA integration to generate all possible badge sizes and types
    - Add example to doc.go
    Clean up lint errors for function comments not conforming in sonarcloud 
    Correct README.md for fossa and add one for gobadges
    Remove swp files

---
+++ fossa/doc.go
@@ -0,0 +1,12 @@
+// Package fossa badge generation
+package fossa
+
+/*
+
+import (
+    fossa "github.com/pavedroad-io/integrations/fossa/cmd"
+)
+
+	embeddedLink := fossa.GetBadge(MarkDown|HTML|Link, Shield|Small|Large, microServiceNmae)
+
+*/
---
+++ go/doc.go
@@ -0,0 +1,16 @@
+// Package fossa badge generation
+package gobadges
+
+/*
+
+import (
+    gobadges "github.com/pavedroad-io/integrations/go/cmd"
+)
+
+	embeddedLink := gobadges.GetGoReportCard(badgeType, orgOrUser, repo)
+
+	Where badgeTypes is
+	- GoReport: link to online report
+	- GoHTMLLink: for embedding in HTML
+	- GoHTMLMarkdown: for embedding in md files
+*/
